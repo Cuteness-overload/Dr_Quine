9e8a7c9248929245b9996f9063dad97169d34ef8
Emma Besnoin <ebesnoin@student.42.fr>
2025-01-09 14:23:37 +0100

    [MOD] Colleen.s working in assemby. However not redirecting correctly. really wierd

--- ASM/ex00/Colleen.s
+++ ASM/ex00/Colleen.s
@@ -1,15 +1,15 @@
+section .data
+msg db "section .data%1$cmsg db %2$c%3$s%2$c, 0, 10%1$csection .text%1$cglobal _start%1$cextern printf%1$c_start:%1$clea rdi, [rel msg]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel msg]%1$cxor rax, rax%1$ccall printf wrt ..plt%1$cmov rax, 60%1$cxor rdi, rdi%1$csyscall%1$c", 0, 10
 section .text
 global _start
 extern printf
 _start:
-lea rdi, [rel str]
-lea rcx, [rel str]
+lea rdi, [rel msg]
 mov rsi, 10
 mov rdx, 34
+lea rcx, [rel msg]
 xor rax, rax
 call printf wrt ..plt
 mov rax, 60
+xor rdi, rdi
 syscall
-ret
-section .rodata
-str: db "section .text%1$cglobal _start%1$cextern printf%1$c_start:%1$clea rdi, [rel str]%1$clea rsi, [rel str]%1$cmov rdx, 10%1$cmov rcx, 34%1$cxor rax, rax%1$ccall printf wrt ..plt%1$cret%1$csection .rodata%1$cstr:%1$cdb %2$c%3$s%2$c",0
