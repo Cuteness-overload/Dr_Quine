5bb3a3a1be3e0fbc8d93315a9c2e9d19cb9ada2a
Emma Besnoin <ebesnoin@student.42.fr>
2025-01-09 14:26:35 +0100

    [MOD] added comments to comply with subject

--- ASM/ex00/Colleen.s
+++ ASM/ex00/Colleen.s
@@ -1,5 +1,6 @@
+;a comment outside
 section .data
-msg db "section .data%1$cmsg db %2$c%3$s%2$c, 0, 10%1$csection .text%1$cglobal _start%1$cextern printf%1$c_start:%1$clea rdi, [rel msg]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel msg]%1$cxor rax, rax%1$ccall printf wrt ..plt%1$cmov rax, 60%1$cxor rdi, rdi%1$csyscall%1$c", 0, 10
+msg db ";a comment outside%1$csection .data%1$cmsg db %2$c%3$s%2$c, 0, 10%1$csection .text%1$cglobal _start%1$cextern printf%1$c_start:%1$clea rdi, [rel msg]%1$cmov rsi, 10%1$cmov rdx, 34%1$c;a comment%1$clea rcx, [rel msg]%1$cxor rax, rax%1$ccall printf wrt ..plt%1$cmov rax, 60%1$cxor rdi, rdi%1$csyscall%1$c", 0, 10
 section .text
 global _start
 extern printf
@@ -7,6 +8,7 @@ _start:
 lea rdi, [rel msg]
 mov rsi, 10
 mov rdx, 34
+;a comment
 lea rcx, [rel msg]
 xor rax, rax
 call printf wrt ..plt
