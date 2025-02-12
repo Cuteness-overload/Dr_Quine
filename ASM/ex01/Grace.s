4b4d68d7860e2407b607f662faa33299252be8bd
Emma Besnoin <ebesnoin@student.42.fr>
2025-02-12 17:26:50 +0100

    [MOD] WTF push/pop rbx??? WHY IS THIS NECESSARY?

--- ASM/ex01/Grace.s
+++ ASM/ex01/Grace.s
@@ -5,11 +5,15 @@ global main
 main:
 mov rdi, fname
 mov rsi, perms
+push rbx
 call fopen
+pop rbx
 push rax
 PRINT
 pop rdi
+push rbx
 call fclose
+pop rbx
 mov rax, 0
 ret
 %endmacro
@@ -25,4 +29,4 @@ START
 section .data
 fname: db "Grace_kid.s", 0
 perms: db "w", 0
-format: db "; A Comment%1$cextern fprintf, fopen, fclose%1$c%%macro START 0%1$cglobal main%1$cmain:%1$cmov rdi, fname%1$cmov rsi, perms%1$ccall fopen%1$cpush rax%1$cPRINT%1$cpop rdi%1$ccall fclose%1$cmov rax, 0%1$cret%1$c%%endmacro%1$c%%macro PRINT 0%1$cmov rdi, rax%1$cmov rsi, format%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, format%1$ccall fprintf%1$c%%endmacro%1$cSTART%1$csection .data%1$cfname: db %2$cGrace_kid.s%2$c, 0%1$cperms: db %2$cw%2$c, 0%1$cformat: db %2$c%3$s%2$c%1$c"
+format: db "; A Comment%1$cextern fprintf, fopen, fclose%1$c%%macro START 0%1$cglobal main%1$cmain:%1$cmov rdi, fname%1$cmov rsi, perms%1$cpush rbx%1$ccall fopen%1$cpop rbx%1$cpush rax%1$cPRINT%1$cpop rdi%1$cpush rbx%1$ccall fclose%1$cpop rbx%1$cmov rax, 0%1$cret%1$c%%endmacro%1$c%%macro PRINT 0%1$cmov rdi, rax%1$cmov rsi, format%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, format%1$ccall fprintf%1$c%%endmacro%1$cSTART%1$csection .data%1$cfname: db %2$cGrace_kid.s%2$c, 0%1$cperms: db %2$cw%2$c, 0%1$cformat: db %2$c%3$s%2$c%1$c"
