0a7f62528752dff1e0f1e3652240ae1b5c04d2a8
Emma Besnoin <infosec@cyberemma.com>
2025-02-12 16:02:36 +0100

    [MOD] smol changes

--- C/ex00/Colleen.c
+++ C/ex00/Colleen.c
@@ -2,7 +2,7 @@
 /*
 commentone
 */
-void colleen(){char*a="#include <stdio.h>%1$c/*%1$ccommentone%1$c*/%1$cvoid colleen(){char*a=%2$c%3$s%2$cc;printf(a,10,34,a);}%1$cint main(){%1$c/*%1$canothercomment%1$c*/%1$ccolleen();return 0;}";printf(a,10,34,a);}
+void colleen(){char*a="#include <stdio.h>%1$c/*%1$ccommentone%1$c*/%1$cvoid colleen(){char*a=%2$c%3$s%2$c;printf(a,10,34,a);}%1$cint main(){%1$c/*%1$canothercomment%1$c*/%1$ccolleen();return 0;}%1$c";printf(a,10,34,a);}
 int main(){
 /*
 anothercomment
--- C/ex01/Grace.c
+++ C/ex01/Grace.c
@@ -2,7 +2,7 @@
 /*
 commentwoohhoo
 */
-#define A(x)int main(){char *a="#include <stdio.h>%1$c/*%1$ccommentwoohhoo%1$c*/%1$c#define A(x)int main(){char *a=%2$c%3$s%2$c;FILE *fptr=fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);fprintf(fptr,a,10,34,a);fclose(fptr);return 0;}%1$c#define B(s)C(s)%1$c#define C(s)#s%1$cA(B(A(x)))";FILE *fptr=fopen("Grace_kid.c", "w");fprintf(fptr,a,10,34,a);fclose(fptr);return 0;}
+#define A(x)int main(){char *a="#include <stdio.h>%1$c/*%1$ccommentwoohhoo%1$c*/%1$c#define A(x)int main(){char *a=%2$c%3$s%2$c;FILE *fptr=fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);fprintf(fptr,a,10,34,a);fclose(fptr);return 0;}%1$c#define B(s)C(s)%1$c#define C(s)#s%1$cA(B(A(x)))%1$c";FILE *fptr=fopen("Grace_kid.c", "w");fprintf(fptr,a,10,34,a);fclose(fptr);return 0;}
 #define B(s)C(s)
 #define C(s)#s
 A(B(A(x)))
